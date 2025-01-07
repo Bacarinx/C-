38c5a6c1daf504ec128d9713f1991d21d81ec8fe
Bacarinx <henriquebacarin@gmail.com>
2025-01-07 16:36:35 -0300

    Reorganizando estrutura da pasta

--- Curso DIO/obj/Debug/net9.0/C#.AssemblyInfo.cs
+++ Curso DIO/obj/Debug/net9.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11965a6022f98f2ad5e5532de89eec77f1bfe536")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d43cdcb23df999d214932e062f2624048b74533")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
