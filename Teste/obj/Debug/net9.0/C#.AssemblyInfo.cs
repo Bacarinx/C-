5c5377da4c2799e996f8bb499b262b846651015c
Bacarinx <henriquebacarin@gmail.com>
2025-01-13 19:06:19 -0300

    creating a new module

--- Teste/obj/Debug/net9.0/C#.AssemblyInfo.cs
+++ Teste/obj/Debug/net9.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e68d0d29dc02d3e1c3b743719400299f1810156d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f0dab3ecf5d5dd411f84c2e655b76c35c435807")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
