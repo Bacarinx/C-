8f3fe991df8c0c70b7f035937d8bc96b0c582767
Bacarin <henriquebacarin@gmail.com>
2025-03-19 18:33:01 -0300

    sla

--- OrderSolution/OrderSolutions.Exception/obj/Debug/net9.0/OrderSolutions.Exception.AssemblyInfo.cs
+++ OrderSolution/OrderSolutions.Exception/obj/Debug/net9.0/OrderSolutions.Exception.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderSolutions.Exception")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4655694f1e78026d8529bd64dcb01fa5a46ce0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d6897634a3b6346c6e825edaab68d0a42e487af")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderSolutions.Exception")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderSolutions.Exception")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
