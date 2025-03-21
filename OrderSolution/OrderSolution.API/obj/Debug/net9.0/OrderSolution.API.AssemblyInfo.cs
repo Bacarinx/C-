db35a996cbf7baf383ca8d82655f9e2069a48156
Bacarin <henriquebacarin@gmail.com>
2025-03-20 22:44:18 -0300

    adding removing product route

--- OrderSolution/OrderSolution.API/obj/Debug/net9.0/OrderSolution.API.AssemblyInfo.cs
+++ OrderSolution/OrderSolution.API/obj/Debug/net9.0/OrderSolution.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderSolution.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4c7f4603960d8408ccc816ae1a81afd3e215892")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+324226f03a738ef8f1f18989c62631d0415322a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderSolution.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderSolution.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
