dce22e36df19f98449363bf30ed27252f1107203
Bacarin <henriquebacarin@gmail.com>
2025-03-24 16:48:40 -0300

    Adding Migrations

--- OrderSolution/OrderSolution.API/obj/Debug/net9.0/OrderSolution.API.AssemblyInfo.cs
+++ OrderSolution/OrderSolution.API/obj/Debug/net9.0/OrderSolution.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderSolution.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c78b376ff844fcb79c8e3ec7c054764c95c2490")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba60b82ffe239ee3493f89d567218dc12557c51f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderSolution.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderSolution.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
